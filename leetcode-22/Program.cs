5341b877870149f95c18aa7906be9ca45583e05c
luorijun <luorijun@outlook.com>
2020-04-10 01:07:51 +0800

    leetcode 22

---
+++ leetcode-22/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace leetcode_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            var result = solution.GenerateParenthesis(3);
+            foreach (var r in result) Console.WriteLine(r);
+        }
+    }
+}
---
+++ leetcode-22/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace leetcode_22
+{
+    public class Solution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var list = new List<string>();
+            DFS(n, 0, 0, "", list);
+            return list;
+        }
+
+        public void DFS(int n, int l, int r, string result, List<string> list)
+        {
+            if (l == n && n == r) list.Add(result);
+            else
+            {
+                if (n > l) DFS(n, l + 1, r, result + "(", list);
+                if (l > r) DFS(n, l, r + 1, result + ")", list);
+            }
+        }
+    }
+}
