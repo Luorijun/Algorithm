0e904437a289293815587ebe840c87ad759edd0c
luorijun <luorijun@outlook.com>
2020-04-10 03:13:17 +0800

    leetcode 300

---
+++ leetcode-300/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode_300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.LengthOfLIS(Array.Empty<int>()));
+            Console.WriteLine(solution.LengthOfLIS(new[] {2}));
+            Console.WriteLine(solution.LengthOfLIS(new[] {2, 2, 2, 2, 2, 2, 2, 2}));
+            Console.WriteLine(solution.LengthOfLIS(new[] {2, 5, 3, 4, 7, 4, 5, 6}));
+            Console.WriteLine(solution.LengthOfLIS(new[] {10, 9, 2, 5, 3, 7, 101, 18}));
+            Console.WriteLine(solution.LengthOfLIS(new[] {7, 8, 9, 70, 2, 3, 4, 5, 6, 7}));
+        }
+    }
+}
---
+++ leetcode-300/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_300
+{
+    public class Solution
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            if (nums.Length < 2) return nums.Length;
+
+            var list = new List<int>(new[] {nums[0]});
+            for (var i = 1; i < nums.Length; i++)
+                if (nums[i] < list.First())
+                    list[0] = nums[i];
+                else if (list.Last() < nums[i])
+                    list.Add(nums[i]);
+                else
+                {
+                    var index = ~list.BinarySearch(nums[i]);
+                    if (index > 0) list[index] = nums[i];
+                }
+            return list.Count;
+        }
+    }
+}
