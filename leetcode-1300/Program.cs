93827f632496b24d2ec5e5d0a60fe73504a4b79e
luorijun <luorijun@outlook.com>
2020-06-14 22:57:53 +0800

    leetcode 1300

---
+++ leetcode-1300/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode_1300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.FindBestValue(new[] {1, 2, 2}, 10));
+            Console.WriteLine(solution.FindBestValue(new[] {4, 9, 3}, 10));
+            Console.WriteLine(solution.FindBestValue(new[] {2, 3, 5}, 10));
+        }
+    }
+}
---
+++ leetcode-1300/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace leetcode_1300
+{
+    public class Solution
+    {
+        public int FindBestValue(int[] arr, int target)
+        {
+            Array.Sort(arr);
+
+            var sum = 0;
+            var result = 0;
+            var prev = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                var curr = sum + result * (arr.Length - i);
+                if (curr == target) break;
+                if (curr - target >= target - prev)
+                {
+                    result--;
+                    break;
+                }
+
+                result++;
+
+                while (i < arr.Length && arr[i] == result)
+                {
+                    sum += arr[i];
+                    i++;
+                }
+
+                if (i == arr.Length) break;
+
+                i--;
+                prev = curr;
+            }
+
+            return result;
+        }
+    }
+}
