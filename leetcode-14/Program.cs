a0baaecf3599be88fe7df1414e982827c7ddde9b
luorijun <luorijun@outlook.com>
2020-06-15 23:11:58 +0800

    leetcode 14

---
+++ leetcode-14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace leetcode_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.LongestCommonPrefix(new string[] { })); // 空字符
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"qwe", "asd", ""})); // 空字符
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"qwe", "qweasd", ""})); // 空字符
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"dog", "racecar", "car"})); // 无匹配
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"flower", "flow", "flight"})); // 有匹配
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"text", "texture", "tex"})); // 有匹配
+            Console.WriteLine(solution.LongestCommonPrefix(new[] {"hahaha", "hahaha", "hahaha"})); // 全匹配
+        }
+    }
+}
---
+++ leetcode-14/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_14
+{
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) return "";
+
+            var r = "";
+            var i = 0;
+            while (true)
+            {
+                var c = ' ';
+                foreach (var s in strs)
+                    if (i == s.Length) return r;
+                    else if (c == ' ') c = s[i];
+                    else if (c != s[i]) return r;
+                r += c;
+                i++;
+            }
+        }
+    }
+}
