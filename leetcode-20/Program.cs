0b43ecf8ff9d2ffca76f22bead8510d9e8896dc7
luorijun <luorijun@outlook.com>
2020-06-17 23:49:19 +0800

    leetcode 20

---
+++ leetcode-20/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.IsValid(""));
+            Console.WriteLine(solution.IsValid("()"));
+            Console.WriteLine(solution.IsValid("()[]{}"));
+            Console.WriteLine(solution.IsValid("(]"));
+            Console.WriteLine(solution.IsValid("([)]"));
+            Console.WriteLine(solution.IsValid("{[]}"));
+        }
+    }
+}
---
+++ leetcode-20/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace leetcode_20
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+
+            foreach (var t in s)
+                switch (t)
+                {
+                    case '{':
+                    case '[':
+                    case '(':
+                        stack.Push(t);
+                        break;
+                    case '}':
+                        if (!stack.TryPop(out var l) || l != '{') return false;
+                        break;
+                    case ']':
+                        if (!stack.TryPop(out var m) || m != '[') return false;
+                        break;
+                    case ')':
+                        if (!stack.TryPop(out var o) || o != '(') return false;
+                        break;
+                }
+
+            return stack.Count == 0;
+        }
+    }
+}
