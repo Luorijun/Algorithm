51381ca2c13fd25e1c16b1c808fa725bfc73d4d2
luorijun <luorijun@outlook.com>
2020-06-18 00:15:51 +0800

    leetcode 1160

---
+++ leetcode-1160/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace leetcode_1160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.CountCharacters(new []{"cat","bt","hat","tree"}, "atach"));
+            Console.WriteLine(solution.CountCharacters(new []{"hello","world","leetcode"}, "welldonehoneyr"));
+        }
+    }
+}
---
+++ leetcode-1160/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_1160
+{
+    public class Solution
+    {
+        public int CountCharacters(string[] words, string chars)
+        {
+            var array = chars.ToCharArray();
+            var length = 0;
+            foreach (var word in words)
+            {
+                var match = true;
+                var s = new List<char>(array);
+                foreach (var t in word)
+                    if (s.Contains(t)) s.Remove(t);
+                    else
+                    {
+                        match = false;
+                        break;
+                    }
+                if (match) length += word.Length;
+            }
+            return length;
+        }
+    }
+}
