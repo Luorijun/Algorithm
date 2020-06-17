d2fcef210084f680fb48c469f53a646aee31acb8
luorijun <luorijun@outlook.com>
2020-06-17 23:06:36 +0800

    leetcode 1014

---
+++ leetcode-1014/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace leetcode_1014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MaxScoreSightseeingPair(new []{8,1,5,2,6}));
+            Console.WriteLine(solution.MaxScoreSightseeingPair(new []{8,1,5,2,6,7,7}));
+        }
+    }
+}
---
+++ leetcode-1014/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace leetcode_1014
+{
+    public class Solution
+    {
+        public int MaxScoreSightseeingPair(int[] A)
+        {
+            var last = A[0];
+            var max = 0;
+            for (var i = 1; i < A.Length; i++)
+            {
+                last = Math.Max(last - 1, A[i - 1] - 1);
+                max = Math.Max(max, A[i] + last);
+            }
+            return max;
+        }
+    }
+}
