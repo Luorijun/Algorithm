29ea4e6c5b916ed6941a732572a9cfad05f22910
luorijun <luorijun@outlook.com>
2020-06-18 23:20:05 +0800

    leetcode 1028

---
+++ leetcode-1028/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace leetcode_1028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.RecoverFromPreorder("1-2--3--4-5--6--7"));
+        }
+    }
+}
---
+++ leetcode-1028/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_1028
+{
+    public class Solution
+    {
+        public TreeNode RecoverFromPreorder(string S)
+        {
+            var stack = new Stack<TreeNode>();
+
+            var last = 0;
+            var deep = 0;
+            var num = "";
+
+            for (var i = 0; i < S.Length; i++)
+                if (S[i] == '-') deep++;
+                else
+                {
+                    num += S[i];
+                    if (i + 1 != S.Length && S[i + 1] != '-') continue;
+                    var node = new TreeNode(int.Parse(num));
+                    if (deep != 0)
+                        if (last < deep) stack.Peek().left = node;
+                        else
+                        {
+                            while (last-- >= deep)
+                                stack.Pop();
+                            stack.Peek().right = node;
+                        }
+
+                    last = deep;
+                    deep = 0;
+                    num = "";
+                    stack.Push(node);
+                }
+
+            return stack.ToList().Last();
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) => val = x;
+    }
+}
