5642e55973f569a1c22f97ec07f67ee56dafb172
luorijun <luorijun@outlook.com>
2020-06-26 23:06:05 +0800

    leetcode 面试题 2.1

---
+++ leetcode-m-2-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace leetcode_m_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var a = new ListNode(123);
+            var b = new ListNode(123);
+            var c = new ListNode(456);
+            var d = new ListNode(456);
+            var e = new ListNode(789);
+            a.next = b;
+            b.next = c;
+            c.next = d;
+            d.next = e;
+
+            Console.WriteLine(solution.RemoveDuplicateNodes(a));
+
+            Console.WriteLine(solution.RemoveDuplicateNodes(null));
+        }
+    }
+}
---
+++ leetcode-m-2-1/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_m_2_1
+{
+    public class Solution
+    {
+        public ListNode RemoveDuplicateNodes(ListNode head)
+        {
+            if (head == null) return null;
+
+            var record = new HashSet<int>(new[] {head.val});
+
+            var node = head.next;
+            var root = new ListNode(head.val);
+            var curr = root;
+
+            while (node != null)
+            {
+                if (!record.Contains(node.val))
+                {
+                    record.Add(node.val);
+                    curr.next = new ListNode(node.val);
+                    curr = curr.next;
+                }
+                node = node.next;
+            }
+            return root;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x) => val = x;
+
+        public override string ToString() => $"{val} -> {next}";
+    }
+}
