a32740316d5ca6cc49353ae7782d243f524b8fad
luorijun <luorijun@outlook.com>
2020-06-26 23:05:37 +0800

    leetcode 面试题 16.18

---
+++ leetcode-m-16-18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leetcode_m_16_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.PatternMatching("", "")); // t
+            Console.WriteLine(solution.PatternMatching("a", "")); // t
+            Console.WriteLine(solution.PatternMatching("a", "zqvamqvuuvvazv")); // t
+            Console.WriteLine(solution.PatternMatching("ab", "")); // f
+            Console.WriteLine(solution.PatternMatching("ab", "asdqwe")); // t
+            Console.WriteLine(solution.PatternMatching("abba", "dogcatcatdog")); // t
+            Console.WriteLine(solution.PatternMatching("abba", "dogcatcatfish")); // f
+            Console.WriteLine(solution.PatternMatching("aaaa", "dogcatcatdog")); // f
+            Console.WriteLine(solution.PatternMatching("abba", "dogdogdogdog")); // t
+            Console.WriteLine(solution.PatternMatching(
+                "bbbbbbbbbbbbbbabbbbb",
+                "ppppppppppppppjsftcleifftfthiehjiheyqkhjfkyfckbtwbelfcgihlrfkrwireflijkjyppppg")); // t
+        }
+    }
+}
---
+++ leetcode-m-16-18/Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace leetcode_m_16_18
+{
+    public class Solution
+    {
+        public bool PatternMatching(string pattern, string value)
+        {
+            if (pattern == "" && value == "") return true;
+            if (pattern.Length == 1) return true;
+            if ((pattern == "ab" || pattern == "ba") && value != "") return true;
+
+            var ca = 0;
+            var cb = 0;
+            foreach (var c in pattern)
+                if (c == 'a') ca++;
+                else cb++;
+
+
+            for (var i = 0; i <= value.Length; i++)
+            {
+                var la = i;
+                var lb = (value.Length - la * ca) / (float) cb;
+
+                if (lb < 0 || Math.Abs(lb % 1) > 0) continue;
+
+                var sa = "";
+                var sb = "";
+
+                var p = 0;
+                foreach (var c in pattern)
+                    switch (c)
+                    {
+                        case 'a' when la != 0:
+                            if (sa == "") sa = value.Substring(p, la);
+                            else if (sa != value.Substring(p, la)) break;
+                            p += la;
+                            break;
+
+                        case 'b' when (int) lb != 0:
+                            if (sb == "") sb = value.Substring(p, (int) lb);
+                            else if (sb != value.Substring(p, (int) lb)) break;
+                            p += (int) lb;
+                            break;
+                    }
+
+                if (p == value.Length && sa != sb) return true;
+            }
+
+            return false;
+        }
+    }
+}
