08c6f1ba2de53a7f45daddb531457dfa1e011e1a
luorijun <luorijun@outlook.com>
2020-06-28 22:13:11 +0800

    leetcode 209

---
+++ leetcode-209/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace leetcode_209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.MinSubArrayLen(7, new int[] { }));
+            Console.WriteLine(solution.MinSubArrayLen(7, new[] {2, 3, 1, 2, 4, 3}));
+            Console.WriteLine(solution.MinSubArrayLen(80, new[] {10, 5, 13, 4, 8, 4, 5, 11, 14, 9, 16, 10, 20, 8}));
+        }
+    }
+}
---
+++ leetcode-209/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode_209
+{
+    public class Solution
+    {
+        public int MinSubArrayLen(int s, int[] nums)
+        {
+            var queue = new Queue<int>();
+            var min = int.MaxValue;
+            var sum = 0;
+            foreach (var t in nums)
+            {
+                sum += t;
+                queue.Enqueue(t);
+
+                while (sum >= s)
+                {
+                    min = Math.Min(min, queue.Count);
+                    sum -= queue.Dequeue();
+                }
+            }
+            return min == int.MaxValue ? 0 : min;
+        }
+    }
+}
