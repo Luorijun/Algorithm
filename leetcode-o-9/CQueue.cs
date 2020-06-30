0d895298079a693a209f6c44fefdbf32b9f17b53
luorijun <luorijun@outlook.com>
2020-06-30 23:01:07 +0800

    leetcode 剑指offer 09

---
+++ leetcode-o-9/CQueue.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace leetcode_o_9
+{
+    public class CQueue
+    {
+        private Stack<int> stackIn;
+        private Stack<int> stackOut;
+
+        public CQueue()
+        {
+            stackIn = new Stack<int>();
+            stackOut = new Stack<int>();
+        }
+
+        public void AppendTail(int value)
+        {
+            stackIn.Push(value);
+        }
+
+        public int DeleteHead()
+        {
+            if (stackOut.TryPop(out var result)) return result;
+            while (stackIn.TryPop(out result)) stackOut.Push(result);
+            if (stackOut.TryPop(out result)) return result;
+            return -1;
+        }
+    }
+}
---
+++ leetcode-o-9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace leetcode_o_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new CQueue();
+            queue.AppendTail(3);
+            Console.WriteLine(queue.DeleteHead());
+            Console.WriteLine(queue.DeleteHead());
+        }
+    }
+}
