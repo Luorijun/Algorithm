49bc278f1e996e3b2804503b5eec879ffef0f17d
luorijun <luorijun@outlook.com>
2020-07-02 23:59:44 +0800

    leetcode 378

---
+++ leetcode-378/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode_378
+{
+    public class Solution
+    {
+        public int KthSmallest(int[][] matrix, int k)
+        {
+            var n = matrix.Length;
+            var l = matrix[0][0];
+            var r = matrix[n - 1][n - 1];
+            while (l < r)
+            {
+                var c = 0;
+                var i = 0;
+                var j = n - 1;
+                var m = l + ((r - l) >> 1);
+                while (i < n && j >= 0)
+                    if (matrix[i][j] <= m) i++;
+                    else
+                    {
+                        j--;
+                        c += i;
+                    }
+                if (c >= k) r = m;
+                else l = m + 1;
+            }
+            return l;
+        }
+    }
+}
