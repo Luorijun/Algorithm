c401e40168baac94790dd9022c199cb5d9bfd896
luorijun <luorijun@outlook.com>
2020-08-16 21:02:36 +0800

    leetcode 733

---
+++ leetcode-733/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace leetcode_733
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            var image = new[]
+            {
+                new[] {1, 1, 1},
+                new[] {1, 1, 0},
+                new[] {1, 0, 1}
+            };
+            foreach (var col in solution.FloodFill(image, 1, 1, 2))
+            {
+                foreach (var i in col)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            image = new[]
+            {
+                new[] {0, 0, 0},
+                new[] {1, 0, 0}
+            };
+            foreach (var col in solution.FloodFill(image, 1, 0, 2))
+            {
+                foreach (var i in col)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ leetcode-733/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace leetcode_733
+{
+    public class Solution
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            var oldColor = image[sr][sc];
+            if (newColor == oldColor) return image;
+
+            var stack = new Stack<int[]>();
+            stack.Push(new[] {sr, sc});
+            while (stack.TryPop(out var pixel))
+            {
+                var x = pixel[0];
+                var y = pixel[1];
+                image[x][y] = newColor;
+                if (x > 0 && image[x - 1][y] == oldColor)
+                    stack.Push(new[] {x - 1, y});
+                if (y > 0 && image[x][y - 1] == oldColor)
+                    stack.Push(new[] {x, y - 1});
+                if (x < image.Length - 1 && image[x + 1][y] == oldColor)
+                    stack.Push(new[] {x + 1, y});
+                if (y < image[0].Length - 1 && image[x][y + 1] == oldColor)
+                    stack.Push(new[] {x, y + 1});
+            }
+
+            return image;
+        }
+    }
+}
