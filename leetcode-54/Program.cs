87e36264b4459c187dea8fa6d60e5f0140fbf857
luorijun <luorijun@outlook.com>
2021-03-15 17:45:35 +0800

    leetcode 54 螺旋矩阵

---
+++ leetcode-54/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace leetcode_54
+{
+    class Program
+    {
+        private static readonly Solution Solution = new();
+
+        static void Main(string[] args)
+        {
+            Test(Array.Empty<int[]>());
+
+            Test(new[]
+            {
+                new[] {1}
+            });
+
+            Test(new[]
+            {
+                new[] {1, 2, 3, 4, 5}
+            });
+
+            Test(new[]
+            {
+                new[] {1, 2},
+                new[] {3, 4}
+            });
+
+            Test(new[]
+            {
+                new[] {1, 2, 3},
+                new[] {4, 5, 6},
+                new[] {7, 8, 9}
+            });
+
+            Test(new[]
+            {
+                new[] {1, 2, 3, 4},
+                new[] {5, 6, 7, 8},
+                new[] {9, 10, 11, 12}
+            });
+
+            Test(new[]
+            {
+                new[] {1},
+                new[] {2},
+                new[] {3},
+                new[] {4},
+                new[] {5},
+            });
+        }
+
+        private static void Test(int[][] input)
+        {
+            var result = Solution.SpiralOrder(input);
+            foreach (var i in result)
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ leetcode-54/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode_54
+{
+    public class Solution
+    {
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+            {
+                return new List<int>();
+            }
+
+            var top = 1;
+            var left = 0;
+            var right = matrix[0].Length - 1;
+            var bottom = matrix.Length - 1;
+            var list = new List<int>();
+
+            var i = -1;
+            var j = 0;
+            var d = 0;
+
+            while (left <= right && (d == 0 || d == 2) || top <= bottom && (d == 1 || d == 3))
+            {
+                switch (d)
+                {
+                    case 0:
+                        if (++i == right)
+                        {
+                            d = 1;
+                            right--;
+                        }
+
+                        break;
+                    case 1:
+                        if (++j == bottom)
+                        {
+                            d = 2;
+                            bottom--;
+                        }
+
+                        break;
+                    case 2:
+                        if (--i == left)
+                        {
+                            d = 3;
+                            left++;
+                        }
+
+                        break;
+                    case 3:
+                        if (--j == top)
+                        {
+                            d = 0;
+                            top++;
+                        }
+
+                        break;
+                }
+
+                list.Add(matrix[j][i]);
+            }
+
+            return list;
+        }
+    }
+}
