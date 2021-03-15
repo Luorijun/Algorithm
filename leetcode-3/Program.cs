b9f3692814ae13da05588dab631dff8acee6fdd5
luorijun <luorijun@outlook.com>
2021-03-15 18:48:58 +0800

    leetcode 3 无重复字符的最长字串

---
+++ leetcode-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace leetcode_3
+{
+    class Program
+    {
+        private static readonly Solution Solution = new();
+
+        static void Main(string[] args)
+        {
+            Test("abcabcbb");
+            Test("bbbbb");
+            Test("pwwkew");
+            Test("");
+            Test("abba");
+        }
+
+        static void Test(string s)
+        {
+            var result = Solution.LengthOfLongestSubstring(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ leetcode-3/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace leetcode_3
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var start = 0;
+            var max = 0;
+
+            var cs = s.ToCharArray();
+            var record = new Dictionary<char, int>();
+            for (var end = 0; end < cs.Length; end++)
+            {
+                var c = cs[end];
+                if (record.ContainsKey(c) && record[c] >= start)
+                    start = record[c] + 1;
+                record[c] = end;
+                max = Math.Max(max, end - start + 1);
+            }
+
+            return max;
+        }
+    }
+}
